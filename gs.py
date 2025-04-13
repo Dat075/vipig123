5cba3125f1e5e815effe44742ce5b9972117d5c4
Dat075 <94795874+Dat075@users.noreply.github.com>
2025-04-13 20:09:10 +0700

    Update gs.py

--- gs.py
+++ gs.py
@@ -7,7 +7,7 @@ import traceback
 import datetime
 
 # Thông tin phiên bản của tool
-VERSION = "1.5.4"
+VERSION = "1.5.5"
 GITHUB_RAW_URL = "https://raw.githubusercontent.com/Dat075/vipig123/refs/heads/main/gs.py"
 GITHUB_VERSION_URL = "https://raw.githubusercontent.com/Dat075/vipig123/refs/heads/main/version.txt"
 
